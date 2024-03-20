cf5235690ce876bff26e810c5ed1ba0ae03078f2
Junior Marcondes <85419536+mrcndz@users.noreply.github.com>
2024-03-20 18:19:35 -0300

    Update select_op_alt.sql

--- select_op_alt.sql
+++ select_op_alt.sql
@@ -1,20 +1,19 @@
 -- GROUP BY/HAVING -
 
 
--- Junção interna - Esta consulta retornará o nome de cada filme e o nome do diretor de cada filme. 
--- A junção interna garante que apenas os filmes que têm diretores associados serão incluídos no resultado.
+-- Junçao interna - Retorna o nome de cada filme e o nome do diretor de cada filme. 
 SELECT M.MOVIE_NAME, CM.CREW_NAME AS DIRECTOR
 FROM MOVIE M
 INNER JOIN DIRECTS D ON M.ID = D.ID_MOVIE
 INNER JOIN CREW_MEMBER CM ON D.ID_CREW = CM.ID;
 
--- Junção externa - Esta consulta retornará uma lista de todos os filmes, juntamente com as críticas dos críticos associadas a cada filme, se disponível. 
--- Se um filme não tiver uma crítica de crítico associada a ele, o campo CRITIC_REVIEW será NULL.
+-- Junção externa - 
 SELECT M.MOVIE_NAME, CR.TEXT AS CRITIC_REVIEW
 FROM MOVIE M
 LEFT JOIN CRITICS_REVIEW CR ON M.ID = CR.ID_MOVIE;
 
--- Semi junção - Esta consulta retornará todos os IDs e apelidos de usuários que assistiram a pelo menos um filme.
+-- Semi junção 
+-- Esta consulta retornará todos os IDs e apelidos de usuários que assistiram a pelo menos um filme.
 SELECT U.ID, U.NICK
 FROM USER_ U
 WHERE EXISTS (
@@ -26,7 +25,8 @@ WHERE EXISTS (
 -- Anti-junção
 
 
--- Subconsulta do tipo escalar - Esta consulta retornará os filmes cuja média de notas é maior do que a média geral das médias de notas dos filmes
+-- Subconsulta do tipo escalar 
+-- Filmes cuja média de notas é maior do que a média geral das médias de notas dos filmes
 SELECT MOVIE_NAME, AVERAGE_GRADE
 FROM MOVIE
 WHERE AVERAGE_GRADE > (
@@ -35,22 +35,31 @@ WHERE AVERAGE_GRADE > (
 );
 
 -- Subconsulta do tipo escalar
+-- Retorna os filmes que tem mesmo ano que o filme de id 9
 SELECT M1.MOVIE_NAME
 FROM MOVIE M1
 WHERE M1.D_YEAR = (
     SELECT M2.D_YEAR
     FROM MOVIE M2
-    WHERE M2.ID = 1
+    WHERE M2.ID = 9
 );
 
 -- Subconsulta do tipo linha
+-- Descrições das listas de filme do usuário de ID = 1 que tem o mesmo nome e data de criação que a lista de Id = 2
+SELECT L.DESCRIPTION_
+FROM LIST_MOVIES L
+WHERE L.ID_USER = 1 AND (L.LIST_NAME, L.CREATION_DATE) = (
+    SELECT L2.LIST_NAME, L2.CREATION_DATE
+    FROM LIST_MOVIES L2
+    WHERE L2.ID = 2
+);
 
--- Subconsulta do tipo tabela
+-- Subconsulta do tipo tabela -- Filmes que tem atores registrados
+SELECT M.NAME
+FROM MOVIE M
+WHERE M.ID IN (
+    SELECT PB.ID
+    FROM DIRECTED PB
+);
 
 -- Operação de conjunto
-
-
-
-
-
-
